5148d98ceee15000dc56edaa11dae7944323ad9c
Abraham <abrahampost@yahoo.com>
2018-12-05 15:37:21 -0500

    REPL doesn't crash on parse error

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 )
 
 var VERSION string = "0.1"
+var hadError bool = false
 
 /*Settings struct Contains the settings for the current interpreter */
 type Settings struct {
@@ -41,9 +42,15 @@ func main() {
 /*RunFile Reads file into biffer and then runs it */
 func RunFile(s Settings) {
 	inputBytes, err := ioutil.ReadFile(s.fileLoc)
-	CheckError(err)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: Unable to read file '%s'\n", s.fileLoc)
+		os.Exit(1)
+	}
 	inputString := string(inputBytes) + "\r\n"
 	Run(inputString, false)
+	if hadError {
+		os.Exit(1)
+	}
 }
 
 /*RunPrompt runs the REPL and feeds input to the run method as it comes in  */
@@ -66,6 +73,7 @@ func RunPrompt() {
 			Run(input, true)
 			input = "" //reset the input for the next run
 		}
+		hadError = false
 	}
 }
 
@@ -75,6 +83,11 @@ func Run(source string, repl bool) {
 	tokens := tokenizer.Tokenize()
 	parser := NewParser(tokens)
 	stmts := parser.Parse()
+
+	if hadError {
+		return
+	}
+
 	interpreter.Interpret(stmts, repl)
 }
 
@@ -93,31 +106,10 @@ func MatchingBraces(input string) bool {
 			}
 			if b == '}' {
 				right_brace++
-				if right_brace > left_brace {
-					//if a right brace has been seen without a left brace coming before it
-					ParseError(-1, "'}' found without matching '{'")
-				}
 			}
 		}
 	}
 	return left_brace == right_brace
-}
-
-/*CheckError checks to see if an error has been reported from a function */
-func CheckError(err error) {
-	if err != nil {
-		ParseError(-1, err.Error())
-	}
-}
-
-/*ParseError Reports an error during the initial tokenization and parsing of the input */
-func ParseError(line int, message string) {
-	var lineMessage string
-	if line != -1 {
-		lineMessage = fmt.Sprintf(" [line %d]", line)
-	}
-	errorMessage := fmt.Sprintf("PARSE_ERROR%s: %s", lineMessage, message)
-	ReportError(errorMessage)
 }
 
 /*RuntimeError stops the execution of the program when it encounters invalid operations duringn the running of the program */
@@ -128,5 +120,5 @@ func RuntimeError(message string) {
 /*ReportError stops execution of the program with a panic-like error message */
 func ReportError(message string) {
 	fmt.Fprintf(os.Stderr, message+"\n")
-	os.Exit(1)
+	hadError = false
 }
